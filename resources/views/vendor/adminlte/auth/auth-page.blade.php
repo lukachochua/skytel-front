85876f05f7e5bd1076a8a6d9947e9a94b735f3fb
lukachochua <91949321+lukachochua@users.noreply.github.com>
2024-08-19 14:11:10 +0400

    style: restyle base adminlte pages

--- resources/views/vendor/adminlte/auth/auth-page.blade.php
+++ resources/views/vendor/adminlte/auth/auth-page.blade.php
@@ -41,7 +41,7 @@
                 @endif
 
                 {{-- Logo Label --}}
-                {!! config('adminlte.logo', '<b>Admin</b>LTE') !!}
+                {!! config('SkyTel', '<b>Sky</b>Tel') !!}
 
             </a>
         </div>
