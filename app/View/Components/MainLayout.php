f9652afc919399d7a67d066cc392ba9064041a7d
lukachochua <91949321+lukachochua@users.noreply.github.com>
2024-09-13 14:27:34 +0400

    chore: pass navbar and footer links variables to the main layout file

--- app/View/Components/MainLayout.php
+++ app/View/Components/MainLayout.php
@@ -2,18 +2,23 @@
 
 namespace App\View\Components;
 
+use App\Models\Link;
 use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
 class MainLayout extends Component
 {
+    public $navbarLinks;
+    public $footerLinks;
+
     /**
      * Create a new component instance.
      */
     public function __construct()
     {
-        //
+        $this->navbarLinks = Link::where('type', 'navbar')->get();
+        $this->footerLinks = Link::where('type', 'footer')->get();
     }
 
     /**
--- app/View/Components/navbar.php
+++ app/View/Components/navbar.php
@@ -10,6 +10,7 @@ use Illuminate\View\Component;
 class Navbar extends Component
 {
     public $navbarLinks;
+
     /**
      * Create a new component instance.
      */
